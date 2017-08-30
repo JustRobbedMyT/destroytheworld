f5b4022999d88f631e98fa3706cbb5362d595410
Justin Turner <TustinJurner@Lola-Lovestein.local>
2017-08-30 18:02:21 -0500

    Hello Butts

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("You got one tiny butt fool.")
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
